1aac187c616970e563ee6c90633657def878c2c5
Geraldo Neto <netogeraldo209@gmail.com>
2023-03-30 01:25:43 -0300

    'preparar'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Usuario } from 'src/shared/model/usuario';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'social';
+  usuario: Usuario;
+
+  constructor(){
+    this.usuario = new Usuario();
+    this.usuario.nome = 'Guguinhaa';
+  }
+  enviar(): void{
+    console.log('uiiii')
+
+  }
 }
---
+++ src/shared/model/usuario.ts
@@ -0,0 +1,5 @@
+export class Usuario {
+    public nome!: string;
+    public cpf!: string;
+    public idade!: number;
+}
